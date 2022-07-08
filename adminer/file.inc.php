3f8bc3a14db450d783555f7e6d073553ad73f610
Roy-Orbison <Roy-Orbison@users.noreply.github.com>
2022-07-08 11:30:07 +0930

    Human-readable compilation
    A working, single-file copy that can be inspected in an editor.

--- adminer/file.inc.php
+++ adminer/file.inc.php
@@ -3,24 +3,24 @@
 
 if ($_GET["file"] == "favicon.ico") {
 	header("Content-Type: image/x-icon");
-	echo lzw_decompress(compile_file('../adminer/static/favicon.ico', 'lzw_compress'));
+	echo base64_decode(compile_file('../adminer/static/favicon.ico', 'base64_encode'));
 } elseif ($_GET["file"] == "default.css") {
 	header("Content-Type: text/css; charset=utf-8");
-	echo lzw_decompress(compile_file('../adminer/static/default.css;../externals/jush/jush.css', 'minify_css'));
+	echo compile_file('../adminer/static/default.css;../externals/jush/jush.css');
 } elseif ($_GET["file"] == "functions.js") {
 	header("Content-Type: text/javascript; charset=utf-8");
-	echo lzw_decompress(compile_file('../adminer/static/functions.js;static/editing.js', 'minify_js'));
+	echo compile_file('../adminer/static/functions.js;static/editing.js');
 } elseif ($_GET["file"] == "jush.js") {
 	header("Content-Type: text/javascript; charset=utf-8");
-	echo lzw_decompress(compile_file('../externals/jush/modules/jush.js;../externals/jush/modules/jush-textarea.js;../externals/jush/modules/jush-txt.js;../externals/jush/modules/jush-js.js;../externals/jush/modules/jush-sql.js;../externals/jush/modules/jush-pgsql.js;../externals/jush/modules/jush-sqlite.js;../externals/jush/modules/jush-mssql.js;../externals/jush/modules/jush-oracle.js;../externals/jush/modules/jush-simpledb.js', 'minify_js'));
+	echo compile_file('../externals/jush/modules/jush.js;../externals/jush/modules/jush-textarea.js;../externals/jush/modules/jush-txt.js;../externals/jush/modules/jush-js.js;../externals/jush/modules/jush-sql.js;../externals/jush/modules/jush-pgsql.js;../externals/jush/modules/jush-sqlite.js;../externals/jush/modules/jush-mssql.js;../externals/jush/modules/jush-oracle.js;../externals/jush/modules/jush-simpledb.js');
 } else {
 	header("Content-Type: image/gif");
 	switch ($_GET["file"]) {
-		case "plus.gif": echo compile_file('../adminer/static/plus.gif'); break;
-		case "cross.gif": echo compile_file('../adminer/static/cross.gif'); break;
-		case "up.gif": echo compile_file('../adminer/static/up.gif'); break;
-		case "down.gif": echo compile_file('../adminer/static/down.gif'); break;
-		case "arrow.gif": echo compile_file('../adminer/static/arrow.gif'); break;
+		case "plus.gif": echo base64_decode(compile_file('../adminer/static/plus.gif', 'base64_encode')); break;
+		case "cross.gif": echo base64_decode(compile_file('../adminer/static/cross.gif', 'base64_encode')); break;
+		case "up.gif": echo base64_decode(compile_file('../adminer/static/up.gif', 'base64_encode')); break;
+		case "down.gif": echo base64_decode(compile_file('../adminer/static/down.gif', 'base64_encode')); break;
+		case "arrow.gif": echo base64_decode(compile_file('../adminer/static/arrow.gif', 'base64_encode')); break;
 	}
 }
 exit;
--- compile.php
+++ compile.php
@@ -13,14 +13,6 @@ function add_apo_slashes($s) {
 	return addcslashes($s, "\\'");
 }
 
-function add_quo_slashes($s) {
-	$return = $s;
-	$return = addcslashes($return, "\n\r\$\"\\");
-	$return = preg_replace('~\0(?![0-7])~', '\\\\0', $return);
-	$return = addcslashes($return, "\0");
-	return $return;
-}
-
 function remove_lang($match) {
 	global $translations;
 	$idf = strtr($match[2], array("\\'" => "'", "\\\\" => "\\"));
@@ -102,43 +94,6 @@ function lang(\$translation, \$number = null) {
 	}
 }
 
-function lzw_compress($string) {
-	// compression
-	$dictionary = array_flip(range("\0", "\xFF"));
-	$word = "";
-	$codes = array();
-	for ($i=0; $i <= strlen($string); $i++) {
-		$x = @$string[$i];
-		if (strlen($x) && isset($dictionary[$word . $x])) {
-			$word .= $x;
-		} elseif ($i) {
-			$codes[] = $dictionary[$word];
-			$dictionary[$word . $x] = count($dictionary);
-			$word = $x;
-		}
-	}
-	// convert codes to binary string
-	$dictionary_count = 256;
-	$bits = 8; // ceil(log($dictionary_count, 2))
-	$return = "";
-	$rest = 0;
-	$rest_length = 0;
-	foreach ($codes as $code) {
-		$rest = ($rest << $bits) + $code;
-		$rest_length += $bits;
-		$dictionary_count++;
-		if ($dictionary_count >> $bits) {
-			$bits++;
-		}
-		while ($rest_length > 7) {
-			$rest_length -= 8;
-			$return .= chr($rest >> $rest_length);
-			$rest &= (1 << $rest_length) - 1;
-		}
-	}
-	return $return . ($rest_length ? chr($rest << (8 - $rest_length)) : "");
-}
-
 function put_file_lang($match) {
 	global $lang_ids, $project, $langs;
 	if ($_SESSION["lang"]) {
@@ -150,33 +105,19 @@ function put_file_lang($match) {
 		$translation_ids = array_flip($lang_ids); // default translation
 		foreach ($translations as $key => $val) {
 			if ($val !== null) {
-				$translation_ids[$lang_ids[$key]] = implode("\t", (array) $val);
+				$translation_ids[$lang_ids[$key]] = $val;
 			}
 		}
 		$return .= '
-		case "' . $lang . '": $compressed = "' . add_quo_slashes(lzw_compress(implode("\n", $translation_ids))) . '"; break;';
+		case "' . $lang . '": return ' . var_representation(array_values($translation_ids)) . ';';
 	}
-	$translations_version = crc32($return);
-	return '$translations = $_SESSION["translations"];
-if ($_SESSION["translations_version"] != ' . $translations_version . ') {
-	$translations = array();
-	$_SESSION["translations_version"] = ' . $translations_version . ';
-}
-
-function get_translations($lang) {
+	return 'function get_translations($lang) {
 	switch ($lang) {' . $return . '
 	}
-	$translations = array();
-	foreach (explode("\n", lzw_decompress($compressed)) as $val) {
-		$translations[] = (strpos($val, "\t") ? explode("\t", $val) : $val);
-	}
-	return $translations;
+	return array();
 }
 
-if (!$translations) {
-	$translations = get_translations($LANG);
-	$_SESSION["translations"] = $translations;
-}
+$translations = get_translations($LANG);
 ';
 }
 
@@ -297,17 +238,6 @@ function php_shrink($input) {
 	return $output;
 }
 
-function minify_css($file) {
-	return lzw_compress(preg_replace('~\s*([:;{},])\s*~', '\1', preg_replace('~/\*.*\*/~sU', '', $file)));
-}
-
-function minify_js($file) {
-	if (function_exists('jsShrink')) {
-		$file = jsShrink($file);
-	}
-	return lzw_compress($file);
-}
-
 function compile_file($match) {
 	global $project;
 	$file = "";
@@ -320,7 +250,7 @@ function compile_file($match) {
 	if ($callback) {
 		$file = call_user_func($callback, $file);
 	}
-	return '"' . add_quo_slashes($file) . '"';
+	return var_representation($file);
 }
 
 if (!function_exists("each")) {
@@ -449,7 +379,6 @@ $file = preg_replace('~"\.\./adminer/static/(functions\.js)"~', $replace, $file)
 $file = preg_replace('~\.\./adminer/static/([^\'"]*)~', '" . h(' . $replace . ') . "', $file);
 $file = preg_replace('~"\.\./externals/jush/modules/(jush\.js)"~', $replace, $file);
 $file = preg_replace("~<\\?php\\s*\\?>\n?|\\?>\n?<\\?php~", '', $file);
-$file = php_shrink($file);
 
 $filename = $project . (preg_match('~-dev$~', $VERSION) ? "" : "-$VERSION") . ($driver ? "-$driver" : "") . ($_SESSION["lang"] ? "-$_SESSION[lang]" : "") . ".php";
 file_put_contents($filename, $file);
